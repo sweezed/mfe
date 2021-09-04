8d40b85bc4d263e87a303ab261c71d023fab5ef0
Edward Sweezey <esweezey@yahoo.com>
2021-09-04 17:11:42 -0400

    trying syntax fix from file

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
+import React from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 
-import Landing from './components/Landing';
-import Pricing from './components/Pricing';
+import Landing from "./components/Landing";
+import Pricing from "./components/Pricing";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'ma'
-})
+  productionPrefix: "ma",
+});
 
 export default () => {
   return (
