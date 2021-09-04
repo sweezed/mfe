cc7030576670b51d7a6c422293a524a822dff788
Edward Sweezey <esweezey@yahoo.com>
2021-09-04 17:21:17 -0400

    trying to fix class classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter} from 'react-router-dom'
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header'
-import { StylesProvider, createGenerateClassName } from '@material-ui/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 import {
   StylesProvider,
-  createGenerateClassName,
+  createGenerateClassName
 } from "@material-ui/core/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: "ma",
+  productionPrefix: 'co'
 });
 
 export default () => {
